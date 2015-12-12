d5ca83dad34a65a2b188521965ee57b093e4df69
rss245 <rss245@gmail.com>
2015-12-11 23:21:23 -0500

    initial codeaddition

---
+++ fpass/src/main/java/com/ddsc/test/ForgotPwdReset.java
@@ -0,0 +1,11 @@
+package com.ddsc.test;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class ForgotPwdReset
+ */
+public class ForgotPwdReset extends MVCPortlet {
+ 
+
+}
